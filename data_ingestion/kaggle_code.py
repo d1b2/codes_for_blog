a91632a4821a0c79ab53b038248c7e038b5e1cf5
Dhruv Bakshi <dhruvbakshi20@gmail.com>
2023-02-16 16:26:46 +0530

    update

--- data_ingestion/kaggle_code.py
+++ data_ingestion/kaggle_code.py
@@ -3,22 +3,28 @@ import json
 from kaggle.api.kaggle_api_extended import KaggleApi
 import re
 
+#Step 1: Create New API token of kaggle
+#Step 2: Create folder named .kaggle and paste kaggle.json in it
+#Step 3: Install Kaggle by running pip install kaggle
 
-
+#Step 4: Getting URL
 
 kaggle_url="https://www.kaggle.com/datasets/uciml/pima-indians-diabetes-database?select=diabetes.csv"
 
+#Step 5: Get filename and dataset name from kaggle_url
 def dataset_and_file_name(kaggle_url:str):
     kaggle_url = re.split(r'[/?=]', kaggle_url)
     file=kaggle_url[-1]
     dataset="/".join(kaggle_url[-4:-2])
     return dataset,file
 
+#Step 6: Getting kaggle username and key from kaggle.json
 def get_kaggle_username_key(kaggle_json_path:str):
     with open(kaggle_json_path) as user:
         user_info=json.load(user)
     return user_info['username'],user_info['key']
 
+#Step6 Authenticate Kaggle API and download dataset at designated path
 def download_kaggle_dataset(path:str):
     username,key=get_kaggle_username_key(".kaggle\kaggle.json") 
     os.environ['KAGGLE_USERNAME'] = username
@@ -29,7 +35,7 @@ def download_kaggle_dataset(path:str):
     dataset,file=dataset_and_file_name(kaggle_url)
     api.dataset_download_file(dataset, file, path)
     
-
+#Call to function
 download_kaggle_dataset('data_ingestion/kaggle_artifacts')
 
 
--- data_ingestion/uci_code.py
+++ data_ingestion/uci_code.py
@@ -1,34 +1,36 @@
 import os
 import urllib.request as request
-from pathlib import Path
 from zipfile import ZipFile
-import numpy as np
 import pandas as pd
 
-
+#Step 1: Getting URL of file
 
 uci_url="https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.zip"
 
+#Step 2: Download raw file
 def download_rawfile(path:str): 
     if not os.path.exists(path):
         os.makedirs(path)
-    filename = os.path.join(path,"data.zip").replace("\\","/")
-    request.urlretrieve(uci_url,filename) 
-    return filename
+    filepath = os.path.join(path,"data.zip").replace("\\","/")
+    request.urlretrieve(uci_url,filepath) 
+    return filepath
 
-
+#Step 3: Unzip raw file contents
 def unzip_raw(path:str,path_to_unzip:str):
     filename=download_rawfile(path)
     with ZipFile(file=filename, mode="r") as zf:
         zf.extractall(path_to_unzip)
 
+#Step 4: Getting headers
+
+#funtion to return list of file paths in unzip directory
 def list_unzip_files(path_to_unzip:str):
     file_list=[]
     for f in os.listdir(path_to_unzip):
         file_list.append(os.path.join(path_to_unzip,f).replace("\\","/"))
     return file_list
 
-    
+#function to return list of desired headers    
 def get_headers(path_to_header:str):
     file1 = open(path_to_header, 'r')
     Lines = file1.readlines()
@@ -39,7 +41,7 @@ def get_headers(path_to_header:str):
     header.append("Spam/No_Spam")
     return header
 
-
+#Step 5: Converting and saving data into desired format
 def saving_data(filelist:list,path_to_csv:str):
     header=get_headers(filelist[-1])
     clean_df=pd.read_csv(filelist[0],sep=",",names=header)
@@ -49,6 +51,7 @@ def saving_data(filelist:list,path_to_csv:str):
     clean_df.to_csv(csvfilepath,index=False)
     return csvfilepath
 
+#Step 6(Optional): Train test split
 def train_test(csvfilepath:str,path_to_train_test:str):
     clean_dataframe=pd.read_csv(csvfilepath)
     if not os.path.exists(path_to_train_test):
@@ -61,13 +64,14 @@ def train_test(csvfilepath:str,path_to_train_test:str):
     train_dataframe.to_csv(train_filepath,index=False)
     test_dataframe.to_csv(test_fielpath,index=False)
 
+#declaring varibles
 path='data_ingestion/uci_artifacts/raw_data'
 path_to_unzip='data_ingestion/uci_artifacts/unzip_data'
 path_to_csv='data_ingestion/uci_artifacts/clean_data'
 path_to_train_test='data_ingestion/uci_artifacts/clean_data/train_test'
 
+#call to functions
 unzip_raw(path=path,path_to_unzip=path_to_unzip)
 filelist=list_unzip_files(path_to_unzip=path_to_unzip)
-
 csvfilepath=saving_data(filelist=filelist,path_to_csv=path_to_csv)
 train_test(csvfilepath=csvfilepath,path_to_train_test=path_to_train_test)
